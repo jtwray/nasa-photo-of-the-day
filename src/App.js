c8b86a49782b52b7ef88046e7fe27ba8613a2513
jtwray <tuckerwray84@gmail.com>
2019-08-27 21:39:22 -0400

    Imports useEffect and useState hooks from React. Makes an Api call to the NasaApi page and pulls down the picture of the day

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+  // Initialize state to hold the image URL
+  const [picture, setPicture] = useState("");
+  const apiKey=`yHw381OIugvbURlMdFPe6KdZ0lG9r9Qd7txnjNox`;
+  const [date, setDate]=useState( "" );
+  const [apiResult, setApiResult]=useState( "" );
+  const [title, setTitle]=useState( "" );
+  console.log(new Date(date))
+  
+  useEffect(() => {
+    // This axios GET request will return a single image
+    axios
+      // .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+      // Which we then set to state
+      .then(res => setPicture(res.data.url))
+      // Always include error handling
+      .catch( error => console.error );
+    
+  }, []);
+
+	return (
+		<div className="App">
+			<div>
+				<button
+					onClick={() => {
+						
+					}}>
+					refreshpictureOfTheDayNasaStyLe
+        </button>
+        <h1>{}</h1>
+				<div>
+					<img src={`${picture}`} alt='Deep Space'></img>
+				</div>
+			</div>
+			Read through the instructions in the README.md file to build your
+			NASA app! Have fun 🚀!
+		</div>
+	);
 }
 
 export default App;
+// `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+// const [picture, setPicture] = useState("");
+// const apiKey = `yHw381OIugvbURlMdFPe6KdZ0lG9r9Qd7txnjNox`;
